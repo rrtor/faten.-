83966f1df86f4d074a150bda73365c32f9e1b81d
rrtor <41769454+rrtor@users.noreply.github.com>
2018-09-04 13:50:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', function(){
+  client.user.setStatus("dnd");
+    var ms = 40000 ;	
+    var setGame = ['ابو عرب حبيب الكل'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
+});
 console.log("Welcome Again !");
  
  client.on("guildMemberRemove", member => {
