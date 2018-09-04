7166af7b66d0713d2d4aaadde49bf2e65d1229ff
rrtor <41769454+rrtor@users.noreply.github.com>
2018-09-04 09:07:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,5 +37,17 @@ client.on("guildMemberAdd", member => {
 })
 
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`is by ha`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
